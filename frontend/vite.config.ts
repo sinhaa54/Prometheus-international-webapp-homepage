251b553bc88da65fef7273561fa0de586a642af3
sinhaa54 <sinhaa54@github.com>
2026-09-25 11:37:53 +0530

    Update vite config and dist index.html

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,11 +3,11 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
-  const base = env.VITE_BASE_PATH || '.';
+  //const base = env.VITE_BASE_PATH || './';
   const apiProxyTarget = env.VITE_API_PROXY_TARGET || 'http://localhost:8000';
 
   return {
-    base,
+    base:'./',
     plugins: [react()],
     server: {
       port: 5173,
